173be1a87645ba0ba2862d98e03970d332c16366
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-03-23 11:11:27 -0400

    fix bug when LDVERSION does not exist (#3)

--- zmq_ops/common.py
+++ zmq_ops/common.py
@@ -31,7 +31,8 @@ def compile():
     ldflags = ' '.join(tf.sysconfig.get_link_flags())
     ext_suffix = get_ext_suffix()
     py_include = '-isystem ' + sysconfig.get_path('include')
-    py_ldflags = sysconfig.get_config_var('LDFLAGS') + ' -lpython' + sysconfig.get_config_var('LDVERSION')
+    py_ldflags = sysconfig.get_config_var('LDFLAGS') + ' -lpython' \
+        + sysconfig.get_config_var('LDVERSION') or sysconfig.get_config_var('VERSION')
     compile_cmd = 'TF_CXXFLAGS="{}" TF_LDFLAGS="{}" EXT_SUFFIX="{}" ' \
         'PYTHON_CXXFLAGS="{}" PYTHON_LDFLAGS="{}" make -C "{}"'.format(
         cxxflags, ldflags, ext_suffix,
